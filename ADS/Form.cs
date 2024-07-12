5c8ec915d6c6bf4fd3ba15d14b8d2c1cbd527716
Elson Vinicius de Souza Lopes <elson.lopes@ndd.tech>
2024-07-12 14:39:56 -0300

    Implementação de algumas melhorias

--- ADS/Form.cs
+++ ADS/Form.cs
@@ -7,34 +7,26 @@ namespace ADS
     {
         FileInfo _fileInfo = new FileInfo(string.Concat(Directory.GetCurrentDirectory(), "\\config.xml"));
         XmlDocument doc = new XmlDocument();
+        XmlHelper xmlHelper = new XmlHelper();
         string folderContent;
         string connectionString = string.Empty;
         string[] xmlFiles;
         bool sucessMessagesShow = false;
-
         public readXml()
         {
             InitializeComponent();
-
             LoadConfig();
-
             CreateOrUpdateConfig();
         }
-
         private void LoadConfig()
         {
             string configXml = File.ReadAllText(_fileInfo.FullName);
             try
             {
-
-
-                doc.LoadXml(configXml);
-
-                textBoxHost.Text = doc.SelectSingleNode("/config/host").InnerText;
-                textBoxBanco.Text = doc.SelectSingleNode("/config/banco").InnerText;
-                textBoxUser.Text = doc.SelectSingleNode("/config/user").InnerText;
-                textBoxPassword.Text = doc.SelectSingleNode("/config/password").InnerText;
-
+                textBoxHost.Text = xmlHelper.GetValueXml("/config/host",configXml);
+                textBoxBanco.Text = xmlHelper.GetValueXml("/config/banco", configXml);
+                textBoxUser.Text = xmlHelper.GetValueXml("/config/user", configXml);
+                textBoxPassword.Text = xmlHelper.GetValueXml("/config/password", configXml);
                 LogMessage("Configuração carregada com sucesso", LogLevel.INFO);
             }
             catch (Exception ex)
@@ -49,7 +41,6 @@ namespace ADS
             string configXml = $@"<config><host>{textBoxHost.Text}</host><banco>{textBoxBanco.Text}</banco><user>{textBoxUser.Text}</user><password>{textBoxPassword.Text}</password></config>";
             if (!File.Exists(path))
                 File.WriteAllText(path, configXml);
-
             _fileInfo = new FileInfo(path);
 
             if (isUpdate)
@@ -93,15 +84,21 @@ namespace ADS
 
                         XmlNode rootNode = doc.DocumentElement;
 
+                        List<string> listarDocInvalid = new List<string>();
+
                         if (rootNode != null)
                         {
-                            if (rootNode.Name == "nfeProc")
+                            switch (rootNode.Name)
                             {
-                                ProcessNFe(file);
-                            }
-                            else if (rootNode.Name == "cteProc")
-                            {
-                                ProcessCTe(file);
+                                case "nfeProc":
+                                    ProcessNFe(file);
+                                    break;
+                                case "cteProc":
+                                    ProcessCTe(file);
+                                    break;
+                                default:
+                                    listarDocInvalid.Add(file);
+                                    break;
                             }
                         }
                     }
@@ -125,7 +122,6 @@ namespace ADS
         public void SaveToDatabaseNFe(XmlNFe values)
         {
             connectionString = string.Format("Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3};", textBoxHost.Text, textBoxBanco.Text, textBoxUser.Text, textBoxPassword.Text);
-
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 string query = "INSERT INTO FerramentaProcXml (chave, cnpjEmissor, cnpjDestinatario, razaoSocialDestinatario, cProd, descricaoProd) VALUES (@Chave, @CNPJEmit, @CNPJdest, @RazaoSocialDestinatario, @CodigoProduto, @NomeProduto)";
@@ -138,7 +134,6 @@ namespace ADS
                     command.Parameters.AddWithValue("@RazaoSocialDestinatario", values.RazaoSocialDestinatario);
                     command.Parameters.AddWithValue("@CodigoProduto", values.CodigoProduto);
                     command.Parameters.AddWithValue("@NomeProduto", values.NomeProduto);
-
                     try
                     {
                         if (!sucessMessagesShow)
@@ -148,7 +143,6 @@ namespace ADS
                         }
                         command.ExecuteNonQuery();
                         LogMessage("Dados da NF-e inseridos com sucesso no banco de dados.", LogLevel.INFO);
-
                     }
                     catch (Exception ex)
                     {
@@ -205,14 +199,12 @@ namespace ADS
                 string xml = File.ReadAllText(file).Replace("xmlns=\"http://www.portalfiscal.inf.br/nfe\"", "").Replace("xmlns=\"http://www.w3.org/2000/09/xmldsig#\"", "");
                 doc.LoadXml(xml);
                 XmlNFe xmlNFe = new XmlNFe();
-
-                xmlNFe.Chave = doc.SelectSingleNode("/nfeProc/protNFe/infProt/chNFe") == null ? string.Empty : doc.SelectSingleNode("/nfeProc/protNFe/infProt/chNFe").InnerText;
-                xmlNFe.CNPJEmit = doc.SelectSingleNode("/nfeProc/NFe/infNFe/emit/CNPJ") == null ? string.Empty : doc.SelectSingleNode("/nfeProc/NFe/infNFe/emit/CNPJ").InnerText;
-                xmlNFe.CnpjDest = doc.SelectSingleNode("/nfeProc/NFe/infNFe/dest/CNPJ") == null ? string.Empty : doc.SelectSingleNode("/nfeProc/NFe/infNFe/dest/CNPJ").InnerText;
-                xmlNFe.RazaoSocialDestinatario = doc.SelectSingleNode("/nfeProc/NFe/infNFe/dest/xNome") == null ? string.Empty : doc.SelectSingleNode("/nfeProc/NFe/infNFe/dest/xNome").InnerText;
-                xmlNFe.CodigoProduto = doc.SelectSingleNode("/nfeProc/NFe/infNFe/det/prod/cProd") == null ? string.Empty : doc.SelectSingleNode("/nfeProc/NFe/infNFe/det/prod/cProd").InnerText;
-                xmlNFe.NomeProduto = doc.SelectSingleNode("/nfeProc/NFe/infNFe/det/prod/xProd") == null ? string.Empty : doc.SelectSingleNode("/nfeProc/NFe/infNFe/det/prod/xProd").InnerText;
-
+                xmlNFe.Chave = xmlHelper.GetValueXml("/nfeProc/protNFe/infProt/chNFe", xml);
+                xmlNFe.CNPJEmit = xmlHelper.GetValueXml("/nfeProc/NFe/infNFe/emit/CNPJ", xml);
+                xmlNFe.CnpjDest = xmlHelper.GetValueXml("/nfeProc/NFe/infNFe/dest/CNPJ", xml);
+                xmlNFe.RazaoSocialDestinatario = xmlHelper.GetValueXml("/nfeProc/NFe/infNFe/dest/xNome", xml);
+                xmlNFe.CodigoProduto = xmlHelper.GetValueXml("/nfeProc/NFe/infNFe/det/prod/cProd", xml);
+                xmlNFe.NomeProduto = xmlHelper.GetValueXml("/nfeProc/NFe/infNFe/det/prod/xProd", xml);
                 SaveToDatabaseNFe(xmlNFe);
                 LogMessage($"Arquivo NF-e {file} processado com sucesso", LogLevel.INFO);
             }
@@ -229,13 +221,11 @@ namespace ADS
                 string xml = RemoverNamespaceCTe(file);
                 XmlCTe xmlCTe = new XmlCTe();
                 doc.LoadXml(xml);
-
-                xmlCTe.Chave = doc.SelectSingleNode("/cteProc/protCTe/infProt/chCTe") == null ? string.Empty : doc.SelectSingleNode("/cteProc/protCTe/infProt/chCTe").InnerText;
-                xmlCTe.CNPJEmit = doc.SelectSingleNode("/cteProc/CTe/infCte/emit/CNPJ") == null ? string.Empty : doc.SelectSingleNode("/cteProc/CTe/infCte/emit/CNPJ").InnerText;
-                xmlCTe.RazaoSocialEmit = doc.SelectSingleNode("/cteProc/CTe/infCte/emit/xNome") == null ? string.Empty : doc.SelectSingleNode("/cteProc/CTe/infCte/emit/xNome").InnerText;
-                xmlCTe.CNPJTomador = doc.SelectSingleNode("/cteProc/CTe/infCte/rem/CNPJ") == null ? string.Empty : doc.SelectSingleNode("/cteProc/CTe/infCte/rem/CNPJ").InnerText;
-                xmlCTe.RazaoSocialTomador = doc.SelectSingleNode("/cteProc/CTe/infCte/rem/xNome") == null ? string.Empty : doc.SelectSingleNode("/cteProc/CTe/infCte/rem/xNome").InnerText;
-
+                xmlCTe.Chave = xmlHelper.GetValueXml("/cteProc/protCTe/infProt/chCTe", xml);
+                xmlCTe.CNPJEmit = xmlHelper.GetValueXml("/cteProc/CTe/infCte/emit/CNPJ", xml);
+                xmlCTe.RazaoSocialEmit = xmlHelper.GetValueXml("/cteProc/CTe/infCte/emit/xNome", xml);
+                xmlCTe.CNPJTomador = xmlHelper.GetValueXml("/cteProc/CTe/infCte/rem/CNPJ", xml);
+                xmlCTe.RazaoSocialTomador = xmlHelper.GetValueXml("/cteProc/CTe/infCte/rem/xNome", xml);
                 SaveToDatabaseCTe(xmlCTe);
                 LogMessage($"Arquivo CT-e {file} processado com sucesso", LogLevel.INFO);
             }
@@ -298,18 +288,14 @@ namespace ADS
         {
             string logFilePath = Path.Combine(Directory.GetCurrentDirectory(), "application.log");
             string logMessage = $"{DateTime.Now} [{level}] : {message}";
-
             const int maxFileSizeInBytes = 5 * 1024 * 1024;
-
             if (File.Exists(logFilePath) && new FileInfo(logFilePath).Length > maxFileSizeInBytes)
             {
                 string arquivFilePath = Path.Combine(Directory.GetCurrentDirectory(), $"application_{DateTime.Now:yyyyMMddHHmmss}.log");
                 File.Move(logFilePath, arquivFilePath);
             }
-
             File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
         }
-
         private void selectedFileConfig_Click(object sender, EventArgs e)
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
@@ -318,21 +304,17 @@ namespace ADS
                 openFileDialog.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
                 openFileDialog.FilterIndex = 1;
                 openFileDialog.RestoreDirectory = true;
-
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     string configFilePath = openFileDialog.FileName;
-
                     try
                     {
                         string configXml = File.ReadAllText(configFilePath);
                         doc.LoadXml(configXml);
-
                         textBoxHost.Text = doc.SelectSingleNode("/config/host").InnerText;
                         textBoxBanco.Text = doc.SelectSingleNode("/config/banco").InnerText;
                         textBoxUser.Text = doc.SelectSingleNode("/config/user").InnerText;
                         textBoxPassword.Text = doc.SelectSingleNode("/config/password").InnerText;
-
                         LogMessage("Configuração carregada com sucesso a partir do arquivo selecionado.", LogLevel.INFO);
                         MessageBox.Show("Configuração carregada com sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
@@ -344,7 +326,6 @@ namespace ADS
                 }
             }
         }
-
         public enum LogLevel
         {
             INFO,
