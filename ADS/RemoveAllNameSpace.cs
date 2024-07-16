2d29a35c049292878fff3bb91e1ba13c263ceaab
Elson Vinicius de Souza Lopes <elson.lopes@ndd.tech>
2024-07-16 11:17:05 -0300

    O inicio da melhoria dos processos

---
+++ ADS/RemoveAllNameSpace.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADS
+{
+    public class RemoveAllNameSpace
+    {
+        public string RemoverNamespaceCTe(string xmlFile)
+        {
+            return File.ReadAllText(xmlFile).Replace("xmlns=\"http://www.portalfiscal.inf.br/cte\"", "").Replace("xmlns=\"http://www.w3.org/2000/09/xmldsig#\"", "");
+        }
+    }
+}
---
+++ ADS/XmlConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADS
+{
+    public class XmlConfig
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public string Banco { get; set; }
+    }
+}
